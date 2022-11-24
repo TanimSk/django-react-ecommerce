fa053e5cc0e0072689adaa8ab7a714f56db71287
tanimsk <sktanim5800@gmail.com>
2022-11-25 00:30:12 +0600

    init1

---
+++ frontend_App/frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+    
+    return (
+        <nav>
+            <ul>
+                <li><strong>Brand</strong></li>
+            </ul>
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#" role="button">Button</a></li>
+            </ul>
+        </nav>
+    );
+}
