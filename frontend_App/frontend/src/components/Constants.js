77365203042613949cadd96be1a93d14a3adef2f
tanimsk <sktanim5800@gmail.com>
2022-12-22 22:48:44 +0600

    prepared env

--- frontend_App/frontend/src/components/Constants.js
+++ frontend_App/frontend/src/components/Constants.js
@@ -1,4 +1,4 @@
 // export const URL = "http://127.0.0.1:8000";
-export const URL = window.location.hostname;
+export const URL = window.location.host;
 
 export const csrf = document.getElementsByName("csrfmiddlewaretoken")[0].value;
