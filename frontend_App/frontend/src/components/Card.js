edee96d4d70075928970e998942944b9de315cf0
tanimsk <sktanim5800@gmail.com>
2022-12-26 13:41:23 +0600

    .

--- frontend_App/frontend/src/components/Card.js
+++ frontend_App/frontend/src/components/Card.js
@@ -6,7 +6,9 @@ export default function Card(props) {
         <Link to={`/product/${props.id}`} style={{ margin: '0 1rem' }}>
             <article style={{ maxWidth: '15rem' }}>
                 <header style={{marginBottom: '.5rem'}}>
-                    <img src={props.imageURL} alt="product image" />
+                    <img src={props.imageURL} alt="product image" style={{
+                        height: '5rem'
+                    }}/>
                 </header>
                 {
                     props.name.length > 17 ? `${props.name.slice(0, 17)}...` : props.name
