d66e796185653027abdacc8489dadf4834793038
tanimsk <sktanim5800@gmail.com>
2022-12-26 13:33:35 +0600

    .

--- all_auth_extended/all_auth_extended.py
+++ all_auth_extended/all_auth_extended.py
@@ -4,4 +4,4 @@ class AccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         # return f'http://localhost:3000/email-verify/{emailconfirmation.key}'
         # return f'http://127.0.0.1:8000/email-verify/{emailconfirmation.key}'
-        return f'https://django-react-ecommerce-production.up.railway.app/{emailconfirmation.key}'
+        return f'https://django-react-ecommerce-production.up.railway.app/email-verify/{emailconfirmation.key}'
